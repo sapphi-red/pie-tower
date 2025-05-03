d5560ad2098996cb54b0b1801574a80427aa0290
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-05-03 23:56:11 +0900

    chore: remove unneeded settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,10 @@
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
-import { fileURLToPath } from 'node:url'
 import unocss from 'unocss/vite'
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      'aggregate-error': fileURLToPath(
-        new URL('./stub/AggregateError.ts', import.meta.url),
-      ),
-    },
-  },
   plugins: [unocss(), solidPlugin()],
   build: {
     target: 'esnext',
-    polyfillDynamicImport: false,
   },
 })
